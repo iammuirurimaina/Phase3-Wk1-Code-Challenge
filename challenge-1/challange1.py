97f9f13fdf743c75d5de5a5d0e7aa32945d3c5e0
ian <ian.maina@student.moringaschool.com>
2023-08-21 07:28:00 +0300

    Converting 12hr to 24 hr time working

---
+++ challenge-1/challange1.py
@@ -0,0 +1,22 @@
+def convert_to_24_hour(hour, minute, meridian):
+    # condition for when it is pm and not noon
+    if meridian == "pm" and hour != 12:
+        # add 12 
+        hour += 12
+    
+    # condition for if it's midnight (12:00 am)
+    if meridian == "am" and hour == 12:
+        hour = 0
+    #convert the hour and minute to strings and add zeros at the begining
+    hours = str(hour).zfill(2)
+    minutes = str(minute).zfill(2)
+    
+
+    time_24_hour = hours + minutes
+    
+    return time_24_hour
+
+# Test cases
+print(convert_to_24_hour(11, 30, "am"))  
+print(convert_to_24_hour(12, 00, "pm"))  
+print(convert_to_24_hour(12, 0, "am"))  

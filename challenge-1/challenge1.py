253138d8567f19d510fbca44b32bc47d0be9aad4
ian <ian.maina@student.moringaschool.com>
2023-08-23 15:59:11 +0300

    Converts input 12-hour format time and converts to 24

---
+++ challenge-1/challenge1.py
@@ -0,0 +1,21 @@
+def time_converter(hour, minute, meridian):
+    if meridian == "pm" and hour != 12:
+        hour += 12
+    
+    if meridian == "am" and hour == 12:
+        hour = 0
+
+    hours = str(hour).zfill(2)
+    minutes = str(minute).zfill(2)
+
+    time_24_hour = hours + minutes
+    
+    return time_24_hour
+#input
+hour_input = int(input("Enter the hour: "))
+minute_input = int(input("Enter the minute: "))
+meridian_input = input("Enter 'am' or 'pm': ")
+
+result = time_converter(hour_input, minute_input, meridian_input)
+print("Time in 24-hour format:", result)
+

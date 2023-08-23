df8dc812db58951fd8749badcbdadfdee101c329
ian <ian.maina@student.moringaschool.com>
2023-08-23 14:36:30 +0300

    Returning positive when only there are only 3 positive integers

--- challenge-2/challenge2.py
+++ challenge-2/challenge2.py
@@ -1,7 +1,15 @@
 def two_positive_digits(num1, num2, num3):
-    if num1 < 0 and num2 < 0 and num3 > 0 :
+    if num1 > 0 and num2 > 0 and num3 < 0 :
         response = True
         return response
+    elif num1 > 0 and num2 < 0 and num3 > 0:
+        response = True
+        return response
+    elif num1 < 0 and num2 > 0 and num3 > 0:
+        response = True
+        return response
+    
+    
     else :
         response = False
         return False
